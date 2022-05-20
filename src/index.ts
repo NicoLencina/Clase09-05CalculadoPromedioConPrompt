cb667afe6c8042fde0282260cfb71443a5e910b8
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-05-20 13:02:56 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-let prom, nt1, nt2, nt3, sumaNota: number;
+let nt1, nt2, nt3, sumaNota: number;
 
-let nombre = prompt("ingrese el nombre de Alumno:");
-console.log("Alumno :" + nombre);
+let nombre = prompt("Ingrese el nombre de Alumno:");
 
 while (nombre !== "") {
-  nt1 = Number(prompt("ingrese calificacion de la Practica"));
-  nt2 = Number(prompt("ingrese calificacion del Problema"));
-  nt3 = Number(prompt("ingrese calificacion del Teorico"));
+  console.log("Alumno :" + nombre);
+  nt1 = Number(prompt("Ingrese calificacion de la Practica"));
+  nt2 = Number(prompt("Ingrese calificacion del Problema"));
+  nt3 = Number(prompt("Ingrese calificacion del Teorico"));
   console.log(
     "Calificacion Practica :" + nt1,
     "Calificacion Problema :" + nt2,
@@ -14,17 +14,16 @@ while (nombre !== "") {
   );
 
   if (nt1 > -1 && nt2 > -1 && nt3 > -1 && nt1 <= 10 && nt2 <= 10 && nt3 <= 10) {
-    prom = console.log(
-      "el promedio de",
-      nombre,
-      "es",
-      nt1 * 0.1 + nt2 * 0.5 + nt3 * 0.4
-    );
+    sumaNota = nt1 * 0.1 + nt2 * 0.5 + nt3 * 0.4;
+    console.log("el promedio de" + nombre + "es" + sumaNota);
   } else {
     console.log("error, usted ingreso un valor errone");
+    console.log(
+      "Controle los valores ingresados: Nota Practica: " + nt1,
+      " Nota Problema: " + nt2,
+      " Nota Teorica: " + nt3
+    );
   }
   nombre = prompt("ingrese el nombre de Alumno:");
-  if (nombre !== "") {
-    console.log("Alumno :" + nombre);
-  }
 }
+console.log("Gracias por usarme XD");
