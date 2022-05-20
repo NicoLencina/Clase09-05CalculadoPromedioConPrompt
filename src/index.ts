b68d5f8e0c186a3e209e16776e42c01f47d0af6b
Lencina Nicolas Angelo <104397441+NicoLencina@users.noreply.github.com>
2022-05-20 13:19:44 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ let nt1, nt2, nt3, sumaNota: number;
 let nombre = prompt("Ingrese el nombre de Alumno:");
 
 while (nombre !== "") {
-  console.log("Alumno :" + nombre);
+  console.log("Alumno: " + nombre);
   nt1 = Number(prompt("Ingrese calificacion de la Practica"));
   nt2 = Number(prompt("Ingrese calificacion del Problema"));
   nt3 = Number(prompt("Ingrese calificacion del Teorico"));
@@ -15,7 +15,7 @@ while (nombre !== "") {
 
   if (nt1 > -1 && nt2 > -1 && nt3 > -1 && nt1 <= 10 && nt2 <= 10 && nt3 <= 10) {
     sumaNota = nt1 * 0.1 + nt2 * 0.5 + nt3 * 0.4;
-    console.log("el promedio de" + nombre + "es" + sumaNota);
+    console.log("el promedio de " + nombre + "es " + sumaNota);
   } else {
     console.log("error, usted ingreso un valor errone");
     console.log(
